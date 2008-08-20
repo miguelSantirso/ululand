5f8c726f97dfea35c4dab6d0f6a53be361439ef8
TiRSO! <TiRSO!@e0e46c49-be69-4f5a-ad62-21024a331aea>
2008-08-20 16:30:53 +0000

    - Últimos cambios para subirlo al servidor
    git-svn-id: http://subversion.assembla.com/svn/ululand@26 e0e46c49-be69-4f5a-ad62-21024a331aea

--- trunk/apps/developers/templates/_title.php
+++ trunk/apps/developers/templates/_title.php
@@ -1,26 +1,6 @@
 <?php use_helper('ulMisc'); ?>
-
-<!--  <div id="logo">
-	<h1><?php echo link_to(__('ulu<span id="land">land</span>'), "@homepage"); ?></h1> 
-	<p><?php echo __('Developers<br/>Network'); ?></p>
-</div>
-
--->
-<!-- 
-<div id="logo">
-	<h1>
-		<a href="http://ululand.com/">ululand.com</a> / <?php echo link_to('developers', '@homepage'); ?> /
-		<?php if($sf_context->getModuleName() != 'home') : ?>
-			<?php echo link_to($sf_context->getModuleName(), $sf_context->getModuleName()); ?> /
-		<?php endif; ?>
-		<?php if($sf_context->getActionName() != 'index') : ?>
-			<?php echo link_to($sf_context->getActionName(), $sf_context->getModuleName().'/'.$sf_context->getActionName()); ?> /
-		<?php endif; ?>
-	</h1>
-</div>
- -->
  
-<div id="title">
+<div id="dynamicTitle">
 	<h1>
 		<?php echo dynamicContextUrl($sf_context); ?>
 	</h1>
--- trunk/web/index.php
+++ trunk/web/index.php
@@ -1,9 +1,26 @@
 <?php
-
+// define the standard symfony environment constants
 define('SF_ROOT_DIR',    realpath(dirname(__FILE__).'/..'));
-define('SF_APP',         'frontend');
 define('SF_ENVIRONMENT', 'prod');
 define('SF_DEBUG',       false);
+
+// get the domain's parts
+list($tld, $domain, $subdomain, $subdomain2) = array_reverse(explode('.', $_SERVER['HTTP_HOST']));
+
+// determine which subdomain we're looking at
+$app = $subdomain;
+$app = (empty($app) || $app == 'www' ) ? 'developers' : $app;
+
+// determine which app to load based on subdomain
+if (!is_dir(SF_ROOT_DIR.'/apps/'.$app))
+{
+    define('SF_APP','developers');
+}
+else
+{
+    define('SF_APP',$app);
+    define('SF_APP','developers');
+}
 
 require_once(SF_ROOT_DIR.DIRECTORY_SEPARATOR.'apps'.DIRECTORY_SEPARATOR.SF_APP.DIRECTORY_SEPARATOR.'config'.DIRECTORY_SEPARATOR.'config.php');
 
